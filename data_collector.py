49acd926c3c4d5f285132622aeb509ca83849abe
Philipp Nolte <phipno@gmail.com>
2025-04-29 01:20:44 +0000

    working version on github codespaces

--- data_collector.py
+++ data_collector.py
@@ -1,8 +1,20 @@
+# *--------------------------------------------------------------------------------------*#
+# *                                                                                .|    *#
+# *     $FILENAME                 data_collector.py    /     (__)          |/            *#
+# *                                                          (oo)------/'   ,__,    ,    *#
+# *     By: $AUTHOR                          phipno       |  (__)     ||    (oo)_____/   *#
+# *                                                             ||---/||    (__)    ||   *#
+# *     Created: $CREATEDAT 2025.04.18    by phipno   |/                  ,    ||--w||   *#
+# *                                                  ,,       !              |'          *#
+# *                                                       ,           ,|             |/  *#
+# *----------------8<------------------[ mooooooo ]--------------------------------------*#
+
 from dotenv import load_dotenv
 import os
 import requests
 import json
 import datetime
+from datetime import datetime
 import time
 import psycopg2
 from psycopg2 import sql
@@ -31,40 +43,43 @@ conn = psycopg2.connect(database=db_name,
                         port=db_port)
 cursor = conn.cursor()
 
-def create_sensor_entry(name, type, units, installation_date, last_seen, is_active):
+def create_sensor_entry(beehive_id, name, type, units, installation_date, last_seen, is_active):
     insert_query = """
-    INSERT INTO sensors (name, location, installation_date, last_inspection_date, notes)
-    VALUES (%s, %s, TO_DATE(%s, %s), TO_DATE(%s, %s), %s)
+    INSERT INTO sensors (beehive_id, sensor_name, sensor_type, measurement_units, installation_date, last_seen, is_active)
+    VALUES (%s, %s, %s, %s, TO_DATE(%s, %s), to_timestamp(%s), %s)
+    RETURNING *
     """
 
     cursor.execute(insert_query, (
+        beehive_id,
         name,
-        location,
+        type,
+        units,
         installation_date[0],
         installation_date[1],
-        last_inspection_date[0],
-        last_inspection_date[1],
-        notes
+        last_seen,
+        is_active
     ))
+    new_row = cursor.fetchone()
     conn.commit()
+    return new_row
 
+# def create_beehive_entry(name, location, installation_date, last_inspection_date, notes):
+#     insert_query = """
+#     INSERT INTO beehives (name, location, installation_date, last_inspection_date, notes)
+#     VALUES (%s, %s, TO_DATE(%s, %s), TO_DATE(%s, %s), %s)
+#     """
 
-def create_beehive_entry(name, location, installation_date, last_inspection_date, notes):
-    insert_query = """
-    INSERT INTO beehives (name, location, installation_date, last_inspection_date, notes)
-    VALUES (%s, %s, TO_DATE(%s, %s), TO_DATE(%s, %s), %s)
-    """
-
-    cursor.execute(insert_query, (
-        name,
-        location,
-        installation_date[0],
-        installation_date[1],
-        last_inspection_date[0],
-        last_inspection_date[1],
-        notes
-    ))
-    conn.commit()
+#     cursor.execute(insert_query, (
+#         name,
+#         location,
+#         installation_date[0],
+#         installation_date[1],
+#         last_inspection_date[0],
+#         last_inspection_date[1],
+#         notes
+#     ))
+#     conn.commit()
 
 # create_beehive_entry(
 #     "Larry",
@@ -88,18 +103,48 @@ def create_beehive_entry(name, location, installation_date, last_inspection_date
 #     "BonBon is the smallest of them all yet the most active"
 # )
 
-def create_data_table(sensor_id, unit, ts, value):
-    if ()
+def create_data_table(table_name):
+    query = sql.SQL("""
+    CREATE TABLE {} (
+            reading_id SERIAL PRIMARY KEY,
+            sensor_id SERIAL REFERENCES sensors(sensor_id),
+            beehive_id SERIAL REFERENCES beehives(beehive_id),
+            measurement_unit VARCHAR(50),
+            ts TIMESTAMP,
+            value FLOAT NOT NULL
+        )
+    """).format(sql.Identifier(table_name))
 
+    cursor.execute(query)
 
-# CREATE TABLE data (
+    conn.commit()
+    return
+
+def insert_values_data(sensor_id, beehive_id, table_name, unit, ts, value):
+    query = sql.SQL("""
+    INSERT INTO {} (sensor_id, beehive_id, measurement_unit, ts, value)
+    VALUES (%s, %s, %s, to_timestamp(%s), %s)
+    """).format(sql.Identifier(table_name))
+
+    cursor.execute(query, (
+        sensor_id,
+        beehive_id,
+        unit, 
+        ts,
+        value
+    ))
+    conn.commit()
+    return
+
+# leave as comment only for reference
+# CREATE TABLE sensor_name_unit (
 #     reading_id SERIAL PRIMARY KEY,
 #     sensor_id SERIAL REFERENCES sensors(sensor_id),
 #     beehive_id SERIAL REFERENCES beehives(beehive_id),
 #     measurement_unit VARCHAR(50),
 #     ts TIMESTAMP,
 #     value FLOAT NOT NULL
-# );
+# ); 
 
 def fetch_beehive_api_paths():
     """Fetch data from the digital beehive API"""
@@ -127,11 +172,26 @@ def fetch_sensor_groups_data():
             # TODO needs error handling or retrying and notifcation
     return responses
     
-def check_varchar_exists(table_name, column_name, value_to_check):
-    query = sql.SQL("SELECT EXISTS(SELECT 1 FROM {} WHERE {} = %s)").format(sql.Identifier(table_name), sql.Identifier(column_name))
-    cursor.execute(query, (value_to_check, ))
-    exists = cursor.fetchone()[0]
-    return exists
+def get_row_by_varchar(table_name, column_name, value_to_check):
+    query = sql.SQL("SELECT * FROM {} WHERE {} = %s").format(
+        sql.Identifier(table_name), 
+        sql.Identifier(column_name)
+    )
+    cursor.execute(query, (value_to_check,))
+    return cursor.fetchone()
+
+def get_table(table_name, shema="public"):
+    """CHeck if a table exists using pg_catalog"""
+    query = """
+        SELECT EXISTS(
+            SELECT 1
+            FROM pg_catalog.pg_tables
+            WHERE schemaname = %s
+            AND tablename = %s
+        );
+    """
+    cursor.execute(query, (shema, table_name))
+    return cursor.fetchone()[0]
 
 def format_for_postgres(data):
     """For later parsing to postgres"""
@@ -149,7 +209,9 @@ def format_for_postgres(data):
             measurement_values = []
             for value in values:
                 measurement_units.append(value)
-                measurement_ts.append(values[value]['ts'])
+                # | !WHY divide by 1000 because 
+                # ↓ ValueError: year 57294 is out of range postgres unix time
+                measurement_ts.append(values[value]['ts'] / 1000)
                 measurement_values.append(values[value]['value'])
             print(sensor_name, "is sensor_type: ", sensor_type)
             print(measurement_units)
@@ -157,19 +219,43 @@ def format_for_postgres(data):
             print(measurement_values)
             #TODO create function that creates postgres entries for each sensor
             #TODO create function that creates postgres entries for each measurement_units
-        units_str = ",".join(str(element) for element in measurement_units)
-        sensor_entrie = check_varchar_exists("sensors", "sensor_name", sensor_name):
-        if (sensor_entrie == False):
-            create_sensor_entry(sensor_name, sensor_type, units_str, )
-            for unit, ts, value in zip(measurement_units, measurement_ts, measurement_values):
-                
-        else 
-            #Insert into already existing datatable 
+        sensor_entry = get_row_by_varchar("sensors", "sensor_name", sensor_name)
+        if (sensor_entry == None):
+            units_str = ",".join(str(element) for element in measurement_units)
+            print("New sensor found! Creating new entry/row in table:sensors, with sensor name: ", sensor_name)
+            sensor_entry = create_sensor_entry(1, sensor_name, sensor_type, units_str, ['2025-04-14', 'YYYY-MM-DD'], measurement_ts[0], True)        
+        for unit, ts, value in zip(measurement_units, measurement_ts, measurement_values):
+            table_name = sensor_name + "_" +  unit
 
-response = fetch_beehive_api_paths()
-if response != None:
-    sensor_entities = parse_api_paths(response)
-print("PARSE: ", sensor_entities)
-data = fetch_sensor_groups_data()
-format_for_postgres(data)
+            data_entry = get_table(table_name)
+
+            print("DATA ENTRY: ", data_entry )
+            if (data_entry == False):
+                print("Nothing found! Creating data_entry called ", table_name)
+                print("SENSOR: ", sensor_entry[0])
+                create_data_table(table_name)
+                # TODO insert directly into freshly created table instead of searching again
+                # data_entry = create_data_table(table_name)
+
+            print("Inserting values into table", table_name)
+            # TODO insert directly into freshly created table instead of searching again
+            insert_values_data(sensor_entry[0], 1, table_name, unit, ts, value)
+
+
+
+loop = True
+while loop:
+    response = fetch_beehive_api_paths()
+    if response != None:
+        sensor_entities = parse_api_paths(response)
+    print("PARSE: ", sensor_entities)
+    data = fetch_sensor_groups_data()
+    format_for_postgres(data)
+    # loop = False
+    time.sleep(150)
+
+
+
 conn.close()
+
+#.~"~._.~"~._.~"~._.~"~.__.~"~._.~"~._.~  EOF  ~._.~"~.__.~"~._.~"~._.~"~._.~"~._.~"~._.~#
