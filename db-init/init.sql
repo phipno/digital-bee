aba18ebafa0611f7267bf8939e5eca6c839793ce
Alphacharge <git.mytechnikinfo.de>
2025-05-12 19:10:06 +0200

    add seperate weatherstaion and mapping table

--- db-init/init.sql
+++ db-init/init.sql
@@ -7,10 +7,17 @@ CREATE TABLE if NOT EXISTS beehives (
     notes TEXT
 );
 
+CREATE TABLE if NOT EXISTS weather_stations (
+    weather_id SERIAL PRIMARY KEY,
+    name VARCHAR(50),
+    location POINT,
+    installation_date DATE,
+    notes TEXT
+);
+
 CREATE TABLE if NOT EXISTS sensors (
     sensor_id SERIAL PRIMARY KEY,
-    beehive_id SERIAL REFERENCES beehives(beehive_id),
-    sensor_name VARCHAR(50),
+    sensor_name VARCHAR(50) UNIQUE,
     sensor_type VARCHAR(50),
     measurement_units TEXT,
     installation_date DATE,
@@ -20,9 +27,16 @@ CREATE TABLE if NOT EXISTS sensors (
 
 CREATE TABLE if NOT EXISTS data (
     reading_id SERIAL PRIMARY KEY,
-    sensor_id SERIAL REFERENCES sensors(sensor_id),
-    beehive_id SERIAL REFERENCES beehives(beehive_id),
+    sensor_id INT REFERENCES sensors(sensor_id),
     measurement_unit VARCHAR(50),
     ts TIMESTAMP,
     value FLOAT NOT NULL
+);
+
+CREATE TABLE if NOT EXISTS sensor_mapping (
+    mapping_id SERIAL PRIMARY KEY,
+    sensor_id INT REFERENCES sensors(sensor_id),
+    beehive_id INT REFERENCES beehives(beehive_id),
+    weather_id INT REFERENCES weather_stations(weather_id),
+    UNIQUE(sensor_id, beehive_id, weather_id)
 );
