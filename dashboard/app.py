8e47a096adcec8b6f58cfd557b956e83c46e794d
phipno <phipno@gmail.com>
2025-05-15 14:01:13 +0200

    multiple select

--- dashboard/app.py
+++ dashboard/app.py
@@ -24,12 +24,17 @@ load_dotenv()
 def get_db_connection():
     try:
         return psycopg2.connect(
-            host=os.getenv('DB_HOST'), # If hosted publicly
+            # host=os.getenv('DB_HOST'), # If hosted publicly
+            host=os.getenv('PG_HOST'), # If hosted locally
             # host=os.getenv('localhost'), # If hosted locally
-            database=os.getenv('DB_NAME'),
-            user=os.getenv('DB_USER'),
-            password=os.getenv('DB_PASSWORD'),
-            port=os.getenv('DB_PORT'),
+            # database=os.getenv('DB_NAME'),
+            database=os.getenv('PG_NAME'),
+            # user=os.getenv('DB_USER'),
+            user=os.getenv('PG_USER'),
+            # password=os.getenv('DB_PASSWORD'),
+            password=os.getenv('PG_PASSWORD'),
+            # port=os.getenv('DB_PORT'),
+            port=os.getenv('PG_PORT'),
             connect_timeout=3,
             application_name="dashboard"  # Add this identifier
         )
